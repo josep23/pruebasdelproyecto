fa6aff542be409e9ada9f9e89691e918ad70f376
Josep <Josep@cuesta>
2021-05-06 17:27:16 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,123 @@
-import java.security.KeyStore;
-import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void tablero(boolean matriz[][]) {
+    public static void mostrarTablero(boolean matriz[][]) {
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                System.out.print(" | " + matriz[i][j] + " | ");
+                if (matriz[i][j]==false){
+                    System.out.print(" | " + " ᕙ(`▿´)ᕗ " + " | ");
+                }
+                else if (matriz[i][j]==true){
+                    System.out.print(" | " + "  (•◡•) / " + " | ");
+                }
+
+                    //System.out.print(" | " + matriz[i][j] + " | ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-            Boolean juego = true;
-            int contador=0;
-            Scanner sc = new Scanner(System.in);
-            int Filas=5;
-            int Columnas=5;
+        Boolean juego = true;
+        int contador = 0;
+        Scanner sc = new Scanner(System.in);
+        int Filas = 5;
+        int Columnas = 5;
 
-            boolean matriz[][] = new boolean[Columnas][Filas];
+        boolean matriz[][] = new boolean[Columnas][Filas];
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                matriz[i][j]=true;
-                System.out.print(" | " + matriz[i][j] + " | ");
+                matriz[i][j] = false;
+                matriz[0][0]=true;
+                matriz[1][0]=true;
+                matriz[2][0]=true;
+                matriz[1][1]=true;
+
+                matriz[1][2]=true;
+                matriz[2][2]=true;
+                matriz[2][1]=true;
+                matriz[2][3]=true;
+                matriz[3][2]=true;
             }
-            System.out.println();
         }
+
+
+        mostrarTablero(matriz);
 
         while (juego != false) {
 
-            System.out.println("/                        /");
+            System.out.println();
             System.out.println("Dime Y");
             int Y = sc.nextInt();
             System.out.println("Dime X");
             int X = sc.nextInt();
 
 
-            if (-1 < Y && Y < matriz.length && -1 < X && X < matriz.length) {
+            if (Y >= 0 && Y < matriz.length && X >= 0 && X < matriz.length) {
 
-                matriz[X][Y] = !matriz[X][Y];
+                matriz[Y][X] = !matriz[Y][X];
 
-                if (X==0){
-                    X=X+1;
+                if (X == 0) {
+                    X = X + 1;
                     matriz[Y][X] = !matriz[Y][X];
-                    X=X-1;
-                }
-                else if (X== matriz.length-1){
-                    X=X-1;
+                    X = X - 1;
+                } else if (X == matriz.length - 1) {
+                    X = X - 1;
                     matriz[Y][X] = !matriz[Y][X];
-                    X=X+1;
-                }
-                else{
-                    X=X-1;
+                    X = X + 1;
+                } else {
+                    X = X - 1;
                     matriz[Y][X] = !matriz[Y][X];
-                    X=X+2;
+                    X = X + 2;
                     matriz[Y][X] = !matriz[Y][X];
-                    X=X-1;
+                    X = X - 1;
                 }
 
-                if (Y==0){
-                    Y=Y+1;
+                if (Y == 0) {
+                    Y = Y + 1;
                     matriz[Y][X] = !matriz[Y][X];
-                    Y=Y-1;
+                    Y = Y - 1;
+                } else if (Y == matriz.length - 1) {
+                    Y = Y - 1;
+                    matriz[Y][X] = !matriz[Y][X];
+                    Y = Y + 1;
+                } else {
+                    Y = Y - 1;
+                    matriz[Y][X] = !matriz[Y][X];
+                    Y = Y + 2;
+                    matriz[Y][X] = !matriz[Y][X];
+                    Y = Y - 1;
                 }
-                else if (Y== matriz.length-1){
-                    Y=Y-1;
-                    matriz[Y][X] = !matriz[Y][X];
-                    Y=Y+1;
-                }
-                else{
-                    Y=Y-1;
-                    matriz[Y][X] = !matriz[Y][X];
-                    Y=Y+2;
-                    matriz[Y][X] = !matriz[Y][X];
-                    Y=Y-1;
-                }
+                contador=0;
                 for (int i = 0; i < matriz.length; i++) {
                     for (int j = 0; j < matriz.length; j++) {
-                        if (matriz[Y][X]=false){
+                        if (matriz[i][j] == false) {
                             contador++;
                         }
                     }
                 }
-                if (contador==25){
-                    System.out.println("GANASTE");
-                    juego=false;
+                if (contador == 25) {
+
+                    System.out.println("██╗░░░██╗██╗░█████╗░████████╗░█████╗░██████╗░██╗░█████╗░");
+                    System.out.println("██║░░░██║██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗");
+                    System.out.println("╚██╗░██╔╝██║██║░░╚═╝░░░██║░░░██║░░██║██████╔╝██║███████║");
+                    System.out.println("░╚████╔╝░██║██║░░██╗░░░██║░░░██║░░██║██╔══██╗██║██╔══██║");
+                    System.out.println("░░╚██╔╝░░██║╚█████╔╝░░░██║░░░╚█████╔╝██║░░██║██║██║░░██║");
+                    System.out.println("░░░╚═╝░░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝");
+                    juego = false;
                 }
-                tablero(matriz);
+
+               else {
+                    mostrarTablero(matriz);
+               }
+
             } else {
                 System.out.println("Coordenadas erroneas");
-                juego=false;
+                juego = false;
             }
         }
     }
